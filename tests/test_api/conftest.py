f5da9c4cde619771c2e1d245536663b235de7960
Evgeny Gordenko <e.gordenko@digital-sector.ru>
2025-10-16 16:37:37 +0300

    add new test in test_api/test_first_api + start pytest, generate allure-report

--- tests/test_api/conftest.py
+++ tests/test_api/conftest.py
@@ -2,5 +2,5 @@ import pytest
 
 
 @pytest.fixture
-def api_url():
+def api_url_petstore():
     return "https://petstore.swagger.io/v2"
--- tests/test_api/test_first_api.py
+++ tests/test_api/test_first_api.py
@@ -1,20 +1,90 @@
+"""
+Страница тестирвоания API покрывающая методы
+Swagger Petstore
+https://petstore.swagger.io/
+"""
+import allure
 import pytest
 import requests
 
-@pytest.mark.xfail
+#@pytest.mark.xfail
+@allure.epic("Пользователи")
+@allure.feature("Создание пользователей")
+@allure.suite("Смоук тесты")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("smoke", "api")
+@allure.label("owner", "QA - evgeny_gord")
+@allure.title("Проверка создания пользователя")
+@allure.description("""
+Этот тест проверяет:
+- API на создание пользователя
+""")
 @pytest.mark.api
-def test_create_user(api_url):
+def test_create_user(api_url_petstore):
     payload_json = {
         "id": 0,
-        "username": "string",
-        "firstName": "string",
-        "lastName": "string",
-        "email": "string",
-        "password": "string",
-        "phone": "string",
-        "userStatus": 0
+        "username": "evgeny_gord",
+        "firstName": "evgeny",
+        "lastName": "gordenko",
+        "email": "jeka21.02.81@mail.ru",
+        "password": "qwerty1234",
+        "phone": "+79999999999",
+        "userStatus": 200
     }
-    response = requests.post(f"{api_url}/user", json=payload_json)
+    response = requests.post(f"{api_url_petstore}/user", json=payload_json)
 
-    assert response.status_code == 201
+    assert response.status_code == 200
     print(response.json())
+
+
+@allure.epic("Пользователи")
+@allure.feature("Авторизация")
+@allure.suite("Смоук тесты")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("smoke", "api")
+@allure.label("owner", "QA - evgeny_gord")
+@allure.title("Проверка авторизации пользователя")
+@allure.description("""
+Этот тест проверяет:
+- API на авторизацию пользователя
+""")
+@pytest.mark.api
+def test_login_user(api_url_petstore):
+    query_params = {
+        "username": "evgeny_gord",
+        "password": "qwerty1234"
+    }
+
+    response = requests.get(f"{api_url_petstore}/user/login", params=query_params)
+
+    assert response.status_code == 200
+    print(response.json())
+
+@allure.epic("Пользователи")
+@allure.feature("Авторизация")
+@allure.suite("Смоук тесты")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("smoke", "api")
+@allure.label("owner", "QA - evgeny_gord")
+@allure.title("Проверка выхода пользователя из системы")
+@allure.description("""
+Этот тест проверяет:
+- API на выход из системы
+""")
+@pytest.mark.api
+def test_logout_user(api_url_petstore):
+    response = requests.get(f"{api_url_petstore}/user/logout")
+    assert response.status_code == 200
+    print(response.json())
+
+
+
+
+
+
+
+
+# pytest -v -k test_name --alluredir=allure-results --clean-alluredir
+# allure generate allure-results -o allure-report --clean
+# allure open allure-report
+# pytest -v test_first.py::test_positive_registration --alluredir=allure-results --clean-alluredir
