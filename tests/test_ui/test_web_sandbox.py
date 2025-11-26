ff3846ed8345dc74fb2bc6a8e57ac743512b3dd6
Evgeny Gordenko <e.gordenko@digital-sector.ru>
2025-11-26 18:01:04 +0300

    new file test_web_sandbox

--- tests/test_ui/test_web_sandbox.py
+++ tests/test_ui/test_web_sandbox.py
@@ -14,6 +14,25 @@ def test_simple_form_registration(browser, base_url_web_sandbox, wait):
         wait.until(EC.visibility_of_element_located((By.ID, "username"))).send_keys("evgeny_gord")
 
     with allure.step("Ввод email"):
+        wait.until(EC.visibility_of_element_located((By.ID, "email"))).send_keys("evgeny_gord@mail.ru")
+
+    with allure.step("Ввод password"):
+        wait.until(EC.visibility_of_element_located((By.ID, "password"))).send_keys("qwerty123")
+
+    with allure.step("Выбор Country"):
+        wait.until(EC.visibility_of_element_located((By.ID, "country"))).click()
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#country>[value='ru']"))).click()
+
+    with allure.step("Соглашение с условиями регистрации"):
+        wait.until(EC.visibility_of_element_located((By.ID, "terms"))).click()
+
+    with allure.step("Нажатие на кнопку Регистрации"):
+        wait.until(EC.visibility_of_element_located((By.ID, "submitBtn"))).click()
+
+    form_result = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#formResult>div>p")))
+    assert 'Форма успешно отправлена!' in form_result.text
+    time.sleep(5)
+
 
 
 
